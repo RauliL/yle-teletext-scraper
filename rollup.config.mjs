3f00cc3543ec52ca216c51dc7fa3d6c74071d5bc
Rauli Laine <rauli.laine@iki.fi>
2024-10-03 20:55:34 +0300

    I forgot to add entities as external dependency

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,6 +12,7 @@ export default [
     },
     external: [
       'cross-fetch',
+      'entities',
       'lodash.get',
       'strip'
     ],
@@ -27,6 +28,7 @@ export default [
     },
     external: [
       'cross-fetch',
+      'entities',
       'lodash.get',
       'strip'
     ],
